b7560eaf72a474345102fc8f9f271f307bf204b6
ZhenyuWu323 <zhw323@lehigh.edu>
2022-08-31 21:56:11 -0400

    java reverselist

--- p1/java/app/src/main/java/edu/lehigh/cse262/p1/App.java
+++ p1/java/app/src/main/java/edu/lehigh/cse262/p1/App.java
@@ -1,5 +1,6 @@
 package edu.lehigh.cse262.p1;
-
+import java.util.List;
+import java.util.ArrayList;
 /**
  * App is the entry point into our program. You are allowed to add fields and
  * methods to App. You may also add `import` statements.
@@ -7,8 +8,15 @@ package edu.lehigh.cse262.p1;
 public class App {
     public static void main(String[] args) {
         System.out.println("CSE 262 Project 1");
-        ReadList reader = new ReadList();
-        System.out.println(reader.read());
+        //ReadList reader = new ReadList();
+        //System.out.println(reader.read());
+        
+        List<String> test = new ArrayList<>();
+        test.add("x");
+        test.add("y");
+        test.add("z");
+        MyReverse list = new MyReverse<>();
+        System.out.println(list.reverse(test));
 
     }
 }
--- p1/java/app/src/main/java/edu/lehigh/cse262/p1/MyReverse.java
+++ p1/java/app/src/main/java/edu/lehigh/cse262/p1/MyReverse.java
@@ -1,6 +1,9 @@
 package edu.lehigh.cse262.p1;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 /** MyReverse is a wrapper class around the function `reverse` */
 public class MyReverse<T> {
@@ -11,6 +14,15 @@ public class MyReverse<T> {
    * @return A list that is the reverse of `in`
    */
   List<T> reverse(List<T> in) {
-    return null;
+    
+    //Use a for loop to go through and reverse the list
+    for(int i = 0, j = in.size() - 1; i < j ; i++){
+      //By adding the last element at the head of list and removing the last element at the same time
+      //which would hold the size the list and reverse the order
+      in.add(i, in.remove(j));
+    }
+
+
+    return in;
   }
 }
