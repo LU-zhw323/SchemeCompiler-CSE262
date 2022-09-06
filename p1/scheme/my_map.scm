5a166daefe5305a30f9047da47c2089e58b847b0
ZhenyuWu323 <zhw323@lehigh.edu>
2022-09-06 19:08:33 -0400

    Zhenyu Wu, scheme: my_map and my_reverse

--- p1/scheme/my_map.scm
+++ p1/scheme/my_map.scm
@@ -5,5 +5,23 @@
 ;; `map` function.
 
 (define (my-map func l)
-  #f ;; [CSE 262] Implement Me!
-  )
+
+  ;;Contributor: Zhenyu Wu
+
+
+  ;;Same idea with my_reverse, just compute func on each iteam for each recursion
+  ;;Basecase where there is only '() left in list, return
+  (if(eq? '() l)
+      '()
+      ;;Recursivly apply the my-map on the 1st element of the list we passed in
+      ;;and join the result with the result of list after recursivly applying the func
+      ;;on the rest of list. Each time, we will compute the func on the 1st element, and tell
+      ;;him to wait for we reach the end. Therefore, the general idea is to take 1 element out from
+      ;;the list each time and apply func on it, and make it on hold waiting for a list returned with result in it
+
+      (cons (func (car l)) (my-map func (cdr l)) )
+  
+  
+  
+  )  
+)
--- p1/scheme/my_reverse.scm
+++ p1/scheme/my_reverse.scm
@@ -7,5 +7,17 @@
 ;; Your implementation should be recursive.
 
 (define (my-reverse l)
-  #f ;; [CSE 262] Implement Me!
+  ;;Contributor：Zhenyu Wu
+
+
+  ;; basecase where there is only null left in list(last element)
+  (if(eq? '() l)
+    ;; return '()
+    '()
+    ;; break the list into two sub-list: 1. list return from the basecase 2. list containing only the 1st element of l
+    ;; then use append to join 2 sub-list for each recursion. since we need to reverse the list, so the return list should be in front of the list
+    ;; of 1st element, therefore, we can't use cons which will make multiple nested list. General idea is to take out the 1st element from the 
+    ;; list passing in, and make it on hold to join the rest of list til each the end of list
+    (append (my-reverse (cdr l)) (list (car l)))
   )
+)
