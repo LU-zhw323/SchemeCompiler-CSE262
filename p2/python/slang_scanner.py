18aaec504b45e7741c080197d5456b18d5fc4585
ZhenyuWu323 <zhw323@lehigh.edu>
2022-09-28 01:00:17 -0400

    python double

--- p2/python/slang_scanner.py
+++ p2/python/slang_scanner.py
@@ -1,7 +1,8 @@
 # [CSE 262] This file is a minimal skeleton for a Scheme scanner in Python.  It
 # provides a transliteration of the TokenStream class, and the shell of a
 # Scanner class.  Please see the README.md file for more discussion.
-
+from lib2to3.pgen2 import token
+import re
 
 class TokenStream:
     def __init__(self, tokens):
@@ -32,9 +33,10 @@ class Scanner:
         ori_text = TokenStream(source)
         #boolean flag to determine state
         PM = False
-        INIT = True
+        ININT = False
         INID = False
         INSTR = False
+        INDOUBLE = False
         Error = False
 
         #Final TokenStream that we will return
@@ -49,25 +51,51 @@ class Scanner:
         while(line_text.hasNext()):
             #Split tokens in each line by \t\r
             inline_token = TokenStream(line_text.nextToken().split())
+            inline = str(line_text.nextNextToken())
+            line_size = 0
             while(inline_token.hasNext()):
                 #Take out tokentext from line of tokens
-                tokenText = TokenStream(inline_token.nextToken())
+                tokenText = str(inline_token.nextToken())
                 temp = []
-                tok = ""
-                while(tokenText.hasNext()):
-                    token = str(tokenText.nextToken())
-                    tok += token
-                    if not token.isdigit():
-                        INIT = False
-                        Error = True
-                    tokenText.popToken()
-                if INIT:
-                    temp = ["Int", line ,col, tok]
+                if tokenText.count('(') != 0:
+                    num = tokenText.count('(')
+                    for i in range(0 ,len(tokenText)):
+                        if(tokenText[i] == '('):
+                            temp = ["LeftParen", line, col+i]
+                            result.append(temp)
+                    col += num
+                    tokenText = tokenText.replace('(','')
+                elif tokenText.count(')') != 0:
+                    num = tokenText.count(')')
+                    for i in range(0 ,len(tokenText)):
+                        if(tokenText[i] == ')'):
+                            temp = ["RightParen", line, col+i]
+                            result.append(temp)
+                    col += num
+                    tokenText = tokenText.replace(')','')
+                
+
+                if(tokenText.isdigit()):
+                    ININT = True
+                elif(tokenText.count('.') == 1 and not tokenText.startswith('.') and not tokenText.endswith('.')):
+                    partial_token = tokenText.split('.')
+                    if(partial_token[0].isdigit() and partial_token[1].isdigit()):
+                        INDOUBLE = True
+                    else: Error = True
+                
+                if ININT:
+                    temp = ["Int", line ,col, tokenText]
+                    ININT = False
+                elif INDOUBLE:
+                    temp = ["Double", line, col, tokenText]
+                    INDOUBLE = False
                 elif Error:
-                    temp = ["Error", line, col, tok]
+                    temp = ["Error", line, col, tokenText]
+                    Error = False
                 result.append(temp)
-                col += 1
                 inline_token.popToken()
+                col += len(tokenText)
+                col += 1
 
             line_text.popToken()
             line += 1
