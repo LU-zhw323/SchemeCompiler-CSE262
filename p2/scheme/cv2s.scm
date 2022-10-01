8f86910fe96dee91e53fc7fb3c1beeb8a9c0b12d
ZhenyuWu323 <zhw323@lehigh.edu>
2022-10-01 18:18:23 -0400

    scheme cv2s

--- p2/scheme/cv2s.scm
+++ p2/scheme/cv2s.scm
@@ -1,2 +1,13 @@
 ;; charvec2string takes a vector of characters and returns a string
-(define (charvec2string cv) "")
+(define (charvec2string cv)
+    (let f((cv cv)
+            (acc ""));f
+        (if(equal? 0 (vector-length cv))
+            acc
+            (f (subvector cv 0 (- (vector-length cv) 1)) (string-append acc (make-string 1 (vector-ref x 0))))
+        )
+            
+    );let
+
+
+);function
