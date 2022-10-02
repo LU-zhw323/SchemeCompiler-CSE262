ee63277bfc890fb8ea3242c6c16b9243fb66d58e
ZhenyuWu323 <zhw323@lehigh.edu>
2022-10-01 21:27:48 -0400

    lv2 comment

--- p2/scheme/l2v.scm
+++ p2/scheme/l2v.scm
@@ -1,20 +1,17 @@
 ;; list2vector takes a list and returns a vector, without using `list->vector`
 (define (list2vector l) 
-    (let f((acc (make-vector (length l) #\a)) 
+    ;;An inner function to perform recursion
+    (let f((acc (make-vector (length l) '())) 
             (l l) 
             (pos 0))
-        (vector-set! acc pos (list-ref l pos))
+        ;;Each time I increment the position index until it hits the last element
         (if (eq? pos (length l))
             acc
-            (f acc l (+ pos 1))
-        )
-    
-    
-    
-    
-    
-    
-    
-    
+            ;;Set the corresponding value in the list to vector, then do recursion
+            (begin
+                (vector-set! acc pos (list-ref l pos))
+                (f acc l (+ pos 1))
+            )
+        ) 
     )
 )
