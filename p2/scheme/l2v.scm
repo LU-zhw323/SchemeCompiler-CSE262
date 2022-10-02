97749e33065a866637fadfac91b9c846bf04a58b
ZhenyuWu323 <zhw323@lehigh.edu>
2022-10-01 21:15:45 -0400

    l2v

--- p2/scheme/l2v.scm
+++ p2/scheme/l2v.scm
@@ -1,13 +1,20 @@
 ;; list2vector takes a list and returns a vector, without using `list->vector`
-(define (list2vector list) 
-    (let f((acc (make-vector (length list) '()))
-            (list list))
-            ;;Base case where we return accumulate string
-            (if(equal? 0 (length list))
-                acc
-                ;;Recursion, each time we add the head of current vector to string and pop out the head from the current string
-                (f () (sublist list 1 (length list)))
-            )
-                
+(define (list2vector l) 
+    (let f((acc (make-vector (length l) #\a)) 
+            (l l) 
+            (pos 0))
+        (vector-set! acc pos (list-ref l pos))
+        (if (eq? pos (length l))
+            acc
+            (f acc l (+ pos 1))
+        )
+    
+    
+    
+    
+    
+    
+    
+    
     )
 )
