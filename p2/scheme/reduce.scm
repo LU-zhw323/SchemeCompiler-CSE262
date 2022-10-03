2df4c1098d0f40cd4cd31dd51196d36f10ae757b
ZhenyuWu323 <zhw323@lehigh.edu>
2022-10-03 17:11:00 -0400

    reduce

--- p2/scheme/reduce.scm
+++ p2/scheme/reduce.scm
@@ -4,4 +4,20 @@
 ;; Example: (reduce + '(1 2 3) 0) ==> 6
 ;;
 ;; Example: (reduce * '() 1) ==> 1
-(define (reduce op l identity) '())
+(define (reduce op l identity)
+    ;;Inner function to do recursion
+    (let f((acc identity);;assign accumulate result to be identity
+            (op op)
+            (l l)
+            (identity identity))
+        ;;basecase where the list is empty, we return accumulate result
+        (if (null? l)
+            acc
+            ;;Each time perform operation on the 1st element in the list, and pass the rest of the list to perform
+            (f (op acc (car l)) op (cdr l) identity)
+           
+        )
+    )
+
+
+)
