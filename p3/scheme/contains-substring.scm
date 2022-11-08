41750f515e5ffb65d76605fa1bd116a2f0128493
ZhenyuWu323 <zhw323@lehigh.edu>
2022-11-07 19:43:22 -0500

    finish contains-string

--- p3/scheme/contains-substring.scm
+++ p3/scheme/contains-substring.scm
@@ -14,19 +14,52 @@
 
 ;; TODO: implement this function
 (define (contains-substring source pattern)
-    (let f((acc "")
-            (source source)
-            (pattern pattern)
-            (flag #f))
-        (if (or (equal? 0 (string-length source)) (equal? 0 (string-length pattern)))
-            acc
-            (if (equal? (string-ref source 0) (string-ref pattern 0))
-                (f (string-append acc (substring source 0 1)) (substring source 1 (string-length source)) (substring pattern 1 (string-length pattern)) #t)
-                (if (equal? flag #t)
-                    (f "" (substring source 1 (string-length source)) (string-append acc pattern) #f)
-                    (f acc (substring source 1 (string-length source)) pattern #f)
-                )
+    ;;create local variable
+    ;;acc -> accumulate string
+    ;;query-> query string
+    (let ((acc "") (query pattern))
+        (begin
+            ;;
+            ;;Inner function that do the recursion
+            ;;it includes flag which used to indicate if we are accumulating the acc or not
+            ;;
+            (let f((acc acc)
+                (source source)
+                (pattern pattern)
+                (flag #f))
+                ;;
+                ;;Basecase where either one of them are "" and we check the content of acc
+                ;;
+                (if (or (equal? 0 (string-length source)) (equal? 0 (string-length pattern)))
+                    (if (equal? acc query)
+                        #t
+                        #f
+                    )
+                    ;;
+                    ;;check if the 1st chararcter of the substring after reducing string is equal
+                    ;;
+                    (if (equal? (string-ref source 0) (string-ref pattern 0))
+                        ;;
+                        ;;if so, reduce both string and starting accumulate acc
+                        ;;
+                        (f (string-append acc (substring source 0 1)) (substring source 1 (string-length source)) (substring pattern 1 (string-length pattern)) #t)
+                        ;;
+                        ;;if not, check if we are currently in the building process
+                        ;;
+                        (if (equal? flag #t)
+                            ;;
+                            ;;if building and the chararcter if not matching, abandon current acc and reform the pattern string
+                            ;;
+                            (f "" (substring source 1 (string-length source)) (string-append acc pattern) #f)
+                            ;;
+                            ;;if not building, just reduce the source string
+                            ;;
+                            (f acc (substring source 1 (string-length source)) pattern #f)
+                        )
+                    )
+                )     
             )
-        )     
+        )
+
     )
 )
