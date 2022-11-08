77a8dae7ed77ad7ae12d450c952d2d88c3767f1a
ZhenyuWu323 <zhw323@lehigh.edu>
2022-11-08 17:56:21 -0500

    simple-hash

--- p3/scheme/simple-hash.scm
+++ p3/scheme/simple-hash.scm
@@ -31,4 +31,98 @@
 
 
 ;; TODO: implement this function
-(define (make-hash size) '())
+(define (make-hash size) 
+    (let ((hash (make-vector size '())))
+
+
+        ;;Hash function perfrom djb2
+        (define (prehash hash str)
+            (let ((hash_val 5831))
+                (let f((hash_val hash_val)
+                    (hash hash)
+                    (str str))
+                    (if (equal? 0 (string-length str))
+                        (modulo hash_val (vector-length hash))
+                        (f (+ (* 33 hash_val) (char->integer (string-ref str 0))) hash (substring str 1 (string-length str)))
+                    )
+                ) 
+            )
+            
+        )
+
+
+
+        ;;Contain function
+        (define (contain hash str)
+            (let ((hash_val (prehash hash str)))
+                (let f((bucket (vector-ref hash hash_val))
+                    (str str))
+                    (if (equal? 0 (length bucket))
+                        #f
+                        (if (equal? str (list-ref bucket 0))
+                            #t
+                            (f (sublist 1 (length bucket)) str)
+                        )
+                    )
+                )
+            )
+        )
+
+        ;;Insert function
+        (define (insert hash str)
+            (let ((hash_val (prehash hash str)))
+                (if (equal? #f (contain hash str))
+                    (begin
+                        (vector-set! hash hash_val (append (vector-ref hash hash_val) (list str)))
+                        (contain hash str)
+                    )
+                    #f
+                )  
+            )
+        )
+
+         ;;Remove function
+        (define (removes hash str)
+            (let ((hash_val (prehash hash str)))
+                (if (equal? #t (contain hash str))
+                    (begin
+                        (vector-set! hash hash_val (deleteItem (vector-ref hash hash_val) str))
+                        #t
+                    )
+                    #f
+                )  
+            )
+        )
+
+    
+        ;;Helper function to remove item from a list
+        (define (deleteItem lst item)
+            (cond ((null? lst)
+                    '())
+                    ((equal? item (car lst))
+                        (cdr lst))
+                    (else
+                        (cons (car lst) 
+                            (deleteItem (cdr lst) item)))))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        ;;lambda expression to call method
+        (lambda(msg arg)
+        (cond
+            ((eq? msg 'insert) (insert hash arg))
+            ((eq? msg 'contains) (contain hash arg))
+            ((eq? msg 'remove) (removes hash arg))
+        )
+        )
+    )
+
+
+)
